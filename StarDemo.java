f8a3568a20ae74bfb5ad4405cedacc0550c46072
Swapnil-197 <sspatil1907@gmail.com>
2024-04-01 18:59:15 +0530

    initial commit

---
+++ StarDemo.java
@@ -0,0 +1,14 @@
+package com.nestedForLoop;
+
+public class StarDemo {
+
+	public static void main(String[] args) {
+		
+		StarPattern sp = new StarPattern();
+		//sp.starMatrix();
+		sp.outerBorder();
+		System.out.println("-----------------");
+		sp.plusSign();
+	}
+
+}
